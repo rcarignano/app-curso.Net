6a1353ffd78e14ae54f2f7c23fa25ca0abd4cfad
Roman David Carignano <romandavidcarignano@gmail.com>
2023-09-25 09:52:16 -0300

    Agrego controllador Provider

---
+++ CBTeam-Mvc/CBTeam-Mvc/Models/ProviderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CBTeam_Mvc.Models
+{
+    public class ProviderModel
+    {
+        public int Id { get; set; } 
+        public string? ProviderName { get; set; }   
+        public string? Locality { get; set; }
+    }
+}
