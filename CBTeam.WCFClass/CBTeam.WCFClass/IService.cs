0efd387040bed7a73d2b0fc4ffb792f943db6fe1
Roman David Carignano <romandavidcarignano@gmail.com>
2023-10-09 13:32:45 -0300

    Cambios agregados al proyecto

---
+++ CBTeam.WCFClass/CBTeam.WCFClass/IService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CBTeam.WCFClass
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        List<Address> GetAddress();
+    }
+
+    [DataContract]
+    public class Address
+    {
+        [DataMember]
+        public string AddressName { get; set; }
+        [DataMember]
+        public int AddressNumber { get; set; }
+
+    }
+}
---
+++ CBTeam.WCFClass/CBTeam.WCFClass/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CBTeam.WCFClass
+{
+    public class Service : IService
+    {
+        List<Address> address = new List<Address>
+        {
+            new Address{AddressName = "Libertad",
+                AddressNumber = 883,},
+            new Address{AddressName = "Canada",
+                AddressNumber = 260,},
+            new Address{AddressName = "Venezuela",
+                AddressNumber = 223,},
+            new Address{AddressName = "1 de Mayo",
+                AddressNumber = 191,},
+            new Address{AddressName = "Jose Hernandez",
+                AddressNumber = 1920,},
+            new Address{AddressName = "Hipolito Irigoyen",
+                AddressNumber = 235,},
+        };
+
+        public List<Address> GetAddress() 
+        { return address; }
+    }
+}
